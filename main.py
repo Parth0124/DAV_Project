00ddf2d15c460de256d11094701223d3c4270c05
Parth0124 <abhangparth@gmail.com>
2025-03-27 00:38:37 +0530

    completed the data alidation component

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
 
 
 if __name__ == '__main__':
@@ -15,5 +16,12 @@ if __name__ == '__main__':
         logging.info("Initiate the data ingestion!")
         data_ingestion_artifact=data_ingestion.initiate_data_ingestion()
         print(data_ingestion_artifact)
+        logging.info("Data ingestion completed!")
+        data_validation_config=DataValidationConfig(training_pipeline_config) 
+        data_validation=DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the data validaition")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        print(data_validation_artifact)
+        logging.info("Data Validation Complete!")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
